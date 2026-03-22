c64ad7216ddee1f7fb1436e01d8e57bfa6056bc0
atomwalk12 <atomwalk12@gmail.com>
2026-03-22 20:34:53 +0100

    Add picom.conf and config.fish

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias rm='trash-put'
+set -x GPTEL_BACKEND "gemini"
+set -x GEMINI_API_KEY "not-set"
--- install.sh
+++ install.sh
@@ -7,15 +7,17 @@ repo_root=$(dirname "$script_path")
 echo "repo: ${repo_root}"
 
 link_dir() {
-  src="$1"
-  dest="$2"
+    src="$1"
+    dest="$2"
 
-  mkdir -p "$(dirname "$dest")"
-  ln -sfn "$src" "$dest"
-  echo "linked: $dest -> $src"
+    mkdir -p "$(dirname "$dest")"
+    ln -sfn "$src" "$dest"
+    echo "linked: $dest -> $src"
 }
 
 # symlinks
 link_dir "${repo_root}/.doom.d" "${HOME}/.doom.d"
 link_dir "${repo_root}/emacs" "${HOME}/.config/emacs"
 link_dir "${repo_root}/config" "${HOME}/.i3/config"
+link_dir "${repo_root}/config.fish" "${HOME}/.config/fish/config.fish"
+link_dir "${repo_root}/picom.conf" "${HOME}/.config/picom.conf"
